fe7484888069aba242a95b7776f9fe42dcfbe5fc
Santhosh-max1 <San967790@gmail.com>
2024-09-19 09:52:16 +0530

    Add files via upload
    task

---
+++ secoundtask.java
@@ -0,0 +1,26 @@
+package worldjava;
+
+public class secoundtask {
+
+	    public static void main(String[] args) {
+	       
+	    	
+	 int a=2, b=3;
+	 if(a<b)
+	 {
+		 System.out.println("this value is positive");
+		 
+	 }
+	 
+	 else
+	 {
+		 System.out.println("this value is nagative");
+	 }
+	    	
+	    	
+	    	
+	    	
+	    }
+	    
+
+}
